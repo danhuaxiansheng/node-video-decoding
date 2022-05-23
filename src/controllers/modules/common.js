b9c29c07661fcd5d14aab6e7e1dff96c1cf6d916
danhuaxiansheng <646042371@qq.com>
2022-05-23 17:19:45 +0800

    tt

--- src/controllers/modules/common.js
+++ src/controllers/modules/common.js
@@ -1,37 +1,18 @@
-const fs = require("fs");
-
-const bookSourceUrl = "https://www.xbiquge.la/modules/article/waps.php";
-
-const {
-  getBooksText,
-  getCatalog,
-  booksListHtml,
-  getVideo,
-} = require("../../utils/common");
-
-// 查询所有书籍信息
-exports.searchBooksList = async (req, res) => {
-  booksListHtml(req.body.keywords, bookSourceUrl).then((result) => {
-    res.send({ code: 200, msg: "查询成功。", data: result.data });
-  });
-};
-
-exports.getCatalog = async (req, res) => {
-  let bookUrl = unescape(req.body.bookUrl || "");
-  getCatalog(bookUrl).then((result) => {
-    res.send({ code: 200, msg: "查询成功。", data: result.data });
-  });
-};
-
-exports.getBooksText = async (req, res) => {
-  let bookUrl = unescape(req.body.bookUrl || "");
-  getBooksText(bookUrl).then((result) => {
-    res.send({ code: 200, msg: "查询成功。", data: result.data });
-  });
-};
+const { getVideo, getVideoList } = require("../../utils/common");
 
 exports.getVideo = async (req, res) => {
   getVideo(unescape(req.body.baseUrl || "")).then((result) => {
+    res.send({ code: 200, msg: "查询成功。", data: result.data });
+  });
+};
+
+exports.getVideoList = async (req, res) => {
+  let params = {
+    key: req.body.key || "",
+  };
+
+  getVideoList(params).then((result) => {
+    debugger;
     res.send({ code: 200, msg: "查询成功。", data: result.data });
   });
 };
--- src/routes/modules/common.js
+++ src/routes/modules/common.js
@@ -1,7 +1,5 @@
 const { Common } = require("../../controllers/index.js");
 module.exports = function (app) {
-  app.post("/api/common/searchBooksList", Common.searchBooksList);
-  app.post("/api/common/getCatalog", Common.getCatalog);
-  app.post("/api/common/getBooksText", Common.getBooksText);
   app.post("/api/common/getVideo", Common.getVideo);
+  app.get("/api/common/getVideoList", Common.getVideoList);
 };
