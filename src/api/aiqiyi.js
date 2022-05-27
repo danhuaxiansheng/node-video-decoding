ab8a362037ff57a7ca949b3f734eb77b948a54e7
danhuaxiansheng <646042371@qq.com>
2022-05-27 16:54:54 +0800

    tijiao

--- src/api/aiqiyi.js
+++ src/api/aiqiyi.js
@@ -25,43 +25,23 @@ exports.getMovieList = async (key) => {
     "https://pcw-api.iqiyi.com/strategy/pcw/data/soBaseCardLeftSide";
 
   let tagNames = ["电影", "电视剧", "动漫", "综艺", "记录片", "脱口秀"];
-  debugger;
+  let siteList = ["爱奇艺", "腾讯", "优酷", "bilibili"];
+
   let params = {
     pageNum: 1,
     key: key,
-    // channel_name: "电影",
-    // duration_level: 0,
-    // need_qc: 0,
-    // site_publish_date_level: null,
-    // site: "iqiyi",
-    // mode: 1,
-    // bitrate: null,
-    // af: 1,
   };
-
-  // 查询全部
-  // pageNum: 1
-  // key: 凡人修仙
-
-  // pageNum: 1
-  // key: 凡人修仙传
-  // channel_name: 电视剧
-  // duration_level: 0
-  // need_qc: 0
-  // site_publish_date_level:
-  // site: iqiyi
-  // mode: 1
-  // bitrate:
-  // af: 1
 
   function getImgUrl(d) {
     let imgSrc = d.g_img;
     if (d.siteName === "爱奇艺") {
       // d.g_img 是默认图片，加上尺寸g_img_size动态请求分辨率会更精准
-      imgSrc = d.g_img.replace(
-        ".jpg",
-        "_" + d.g_img_size.split(",")[0] + ".jpg"
-      );
+      if (d.g_img_size) {
+        imgSrc = d.g_img.replace(
+          ".jpg",
+          "_" + d.g_img_size.split(",")[0] + ".jpg"
+        );
+      }
     }
     return imgSrc;
   }
@@ -74,11 +54,12 @@ exports.getMovieList = async (key) => {
       .then((result) => {
         let formatData = JSON.parse(result).data?.formatData ?? {};
         let list = formatData?.list ?? [];
-        let data = list.filter((d) => tagNames.includes(d.tag));
+        let data = list.filter(
+          (d) => tagNames.includes(d.tag) && siteList.includes(d.siteName)
+        );
         data = data.map((d) => {
           // d.g_img 是默认图片，加上尺寸g_img_size动态请求分辨率会更精准
           let imgSrc = getImgUrl(d);
-
           return {
             desc: d.desc, //描述;
             actor: d.actor, // 主演
--- src/controllers/modules/common.js
+++ src/controllers/modules/common.js
@@ -1,15 +1,27 @@
-const { getVideo, getVideoAnalysis, getDataList } = require("../../utils/common");
+const {
+  getVideo,
+  getVideoAnalysis,
+  getDataList,
+} = require("../../utils/common");
 
 exports.getVideo = async (req, res) => {
-  getVideo(decodeURI(req.body.url || "")).then((result) => {
-    res.send({ code: 200, msg: "查询成功。", data: result.data });
-  });
+  getVideo(decodeURI(req.body.url || ""))
+    .then((result) => {
+      res.send({ code: 200, msg: "查询成功。", data: result.data });
+    })
+    .catch((d) => {
+      res.send({ code: 500, msg: "接口查询失败。" });
+    });
 };
 
 exports.getVideoAnalysis = async (req, res) => {
-  getVideoAnalysis(decodeURI(req.body.url || "")).then((result) => {
-    res.send({ code: 200, msg: "查询成功。", data: result.data });
-  });
+  getVideoAnalysis(decodeURI(req.body.url || ""))
+    .then((result) => {
+      res.send({ code: 200, msg: "查询成功。", data: result.data });
+    })
+    .catch((d) => {
+      res.send({ code: 500, msg: "接口查询失败。" });
+    });
 };
 
 exports.getDataList = async (req, res) => {
