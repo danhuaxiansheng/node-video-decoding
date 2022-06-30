63da7ded427c8da2fb024105ec9e32fff05ce458
danhuaxiansheng <646042371@qq.com>
2022-06-30 15:42:03 +0800

    提交路由‘

--- index.js
+++ index.js
@@ -2,17 +2,31 @@
 const multer = require("multer");
 // 引入body-parser中间件，用来处理post请求体body中的数据
 const bodyParser = require("body-parser");
-
-const path = require("path");
-const express = require("express");
 // 处理跨域
 const cors = require("cors");
+
+const path = require("path");
+
+//引入express框架	返回值其实是一个方法
+//再创建服务器就不需要再引用http模块的createHttp方法了。
+const express = require("express");
 
 const app = express();
 const http = require("http");
 
-const { port } = require("./src/setting");
+// 允许跨域
+app.use(cors());
+// 读取静态资源
+app.use(express.static(path.join(__dirname, "public")));
+// parse requests of content-type - application/json
+app.use(bodyParser.json({ limit: "50mb" }));
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
+// 通过配置multer的dest属性， 将文件储存在项目下的tmp文件中
+app.use(multer({ dest: "./public/temp/" }).any());
+
+const { port } = require("./src/setting");
 app.set("port", port || 3000);
 
 //设置跨域访问
@@ -32,18 +46,7 @@ app.all("*", function (req, res, next) {
   else next();
 });
 
-// 允许跨域
-app.use(cors());
-// 读取静态资源
-app.use(express.static(path.join(__dirname, "public")));
-// parse requests of content-type - application/json
-app.use(bodyParser.json({ limit: "50mb" }));
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
-// 通过配置multer的dest属性， 将文件储存在项目下的tmp文件中
-app.use(multer({ dest: "./public/temp/" }).any());
-
+// 加载路由
 require("./src/routes/index.js")(app);
 
 http.createServer(app).listen(app.get("port"), function () {
