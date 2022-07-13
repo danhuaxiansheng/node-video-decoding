a17a807597f3753c6049a8949ac9a6e8037b0f46
danhauxiansheng <646042371@qq.com>
2022-07-13 20:17:54 +0800

    【*】新增打包命令行

--- src/utils/test.js
+++ src/utils/test.js
@@ -3,8 +3,6 @@
 // const superagent = require('superagent')
 //Selenium 自动化测试
 
-
-
   // "http://okjx.cc/?url=", // 有效 但资源不是最新
 // http://www.gddyu.com/ --够低调网
 // https://m3u8.okjx.cc:3389/m13.php?url=https://www.iqiyi.com/v_1pdntwc66rs.html
@@ -13,6 +11,33 @@
 // 解析b站视频   优酷
 // https://jx.parwix.com:4433/player/analysis.php?v=https://www.bilibili.com/bangumi/play/ep409856/
 // https://jx.parwix.com:4433/player/?url=https://www.bilibili.com/bangumi/play/ep409856/
+
+// 解析 爱奇艺视频  -优酷
+// https://api.okjx.cc:3389/jx.php?url=https://www.iqiyi.com/v_26cxipz70mw.html?vfrm=pcw_playpage&vfrmblk=D&vfrmrst=80521_listbox_positive
+
+// (function anonymous() {
+//   setInterval(function () {
+//     check();
+//   }, 2000);
+
+//   var check = function () {
+//     function doCheck(a) {
+//       if (("" + a / a)["length"] !== 1 || a % 20 === 0) {
+//         (function () {}["constructor"]("debugger")());
+//       } else {
+//         (function () {}["constructor"]("debugger")());
+//       }
+//       doCheck(++a);
+//     }
+//     try {
+//       doCheck(0);
+//     } catch (err) {}
+//   };
+//   check();
+// });
+
+// 解析b站视频   优酷
+// https://jx.parwix.com:4433/player/analysis.php?v=https://www.bilibili.com/bangumi/play/ep409856/
 
 // 解析 爱奇艺视频  -优酷
 // https://api.okjx.cc:3389/jx.php?url=https://www.iqiyi.com/v_26cxipz70mw.html?vfrm=pcw_playpage&vfrmblk=D&vfrmrst=80521_listbox_positive
